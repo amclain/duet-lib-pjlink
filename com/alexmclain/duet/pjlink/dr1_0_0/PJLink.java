4f7cdf6de9f918b89277acb0e14c34784e495291
Alex McLain <alexm@morgansound.com>
2012-11-20 11:38:16 -0800

    Increased timeout doesn't help. Reduced back to 4s. Flush the socket writer before the thread sleeps, just in case.

--- com/alexmclain/duet/pjlink/dr1_0_0/PJLink.java
+++ com/alexmclain/duet/pjlink/dr1_0_0/PJLink.java
@@ -483,7 +483,7 @@ public class PJLink {
 						notifyListeners(new PJLinkEvent(PJLinkEvent.EVENT_ERROR, PJLink.ERROR_CONNECTION));
 						this.cancel();
 					}
-				}, 10000);
+				}, 4000);
 				
 				try {
 					connect();
@@ -497,6 +497,8 @@ public class PJLink {
 					else {
 						_socketWriter.println(command);
 					}
+					
+					_socketWriter.flush();
 					
 					while (_socketCommandReceived == false) Thread.yield();
 				}
